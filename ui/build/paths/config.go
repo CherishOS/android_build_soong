44f617b70a1f6656d5a59b59d88c5232c8d648aa
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 23:13:38 -0300

    soong: Add 'repo' to allowed commands
    Change-Id: I49927cf3c3b08bd9e744dfbd88e6269681c8057d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -108,6 +108,7 @@ var Configuration = map[string]PathConfig{
 	"tr":          Allowed,
 	"unzip":       Allowed,
 	"zip":         Allowed,
+        "repo":        Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
