e20854304323004033397fe24f052f6e40411385
Jan Altensen <info@stricted.net>
2020-09-10 15:21:40 +0200

    soong: allow overriding header files
    Includes:
    
      Author: Jan Altensen <info@stricted.net>
      Date:   Sat Aug 7 19:41:59 2021 +0200
    
        soong: move header override to compiler.go
    
         * library.go only covers libraries
    
        Change-Id: I3374999d6b364dd1bbc2060996964ee7b04493e7
    
    Change-Id: Ia9d2210605c5927b529fbe9485b0e5abd079f487
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -351,6 +351,8 @@ type ProductVariables struct {
 
 	DeviceKernelHeaders []string `json:",omitempty"`
 
+	TargetSpecificHeaderPath *string `json:",omitempty"`
+
 	ExtraVndkVersions []string `json:",omitempty"`
 
 	NamespacesToExport []string `json:",omitempty"`
