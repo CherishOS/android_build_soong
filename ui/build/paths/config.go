541868e795bff085537629a403bd86e358eb5adb
Lucchetto <lucchetto.tie@live.com>
2020-09-10 06:24:14 +0200

    soong: whitelist brotli
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Iae3c1360a4694cd988d89d401ef18c75320c2abb
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -76,6 +76,7 @@ func GetConfig(name string) PathConfig {
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
 	"ccache":  Allowed,
+	"brotli":  Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
