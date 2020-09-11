f5c2f2a944cc4073aa89e931c9512213b569fdec
dianlujitao <dianlujitao@lineageos.org>
2020-09-11 11:41:51 +0800

    soong: Hack to preserve cherish product variables
    Change-Id: Ice4093b86baed6974bab630f0e80f6b13a5e28f3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -988,6 +988,11 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.cherish") {
+				// Convert Product_variables.cherish.Foo to cherish.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field
