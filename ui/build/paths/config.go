bd7bdb68cd475100b0d714b194a017ee833cb873
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-12 18:11:56 +0200

    soong: Whitelist brotli
    Change-Id: I06d255b9098dcf4189e7fc3c163c5f6a2ebc0dc2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -115,6 +115,7 @@ var Configuration = map[string]PathConfig{
 	"python3.8":   Allowed,
         "locale":      Allowed,
 	"ccache":      Allowed,
+	"brotli":      Allowed,
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
