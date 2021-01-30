116e7ee6069d0e573a20e123a67cf016815d9b9c
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-30 20:08:32 +0530

    Replace device_kernel_headers with generated_kernel_headers
    * For inline kernel building
    * Avoids having to make edits to multiple repos, even
      if it's a quick replacement
    
    Change-Id: I01d4a9b3e24315731efbc8d16882818d20e38e89
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- cc/cc.go
+++ cc/cc.go
@@ -2943,6 +2943,22 @@ func (c *Module) DepsMutator(actx android.BottomUpMutatorContext) {
 		deps.ReexportSharedLibHeaders, _ = FilterNdkLibs(c, ctx.Config(), deps.ReexportSharedLibHeaders)
 	}
 
+
+                rewriteHeaderLibs := func(list []string) (newHeaderLibs []string) {
+                        newHeaderLibs = []string{}
+                        for _, entry := range list {
+                                // Replace device_kernel_headers with generated_kernel_headers
+                                // for inline kernel building
+                                if entry == "device_kernel_headers" {
+                                        newHeaderLibs = append(newHeaderLibs, "generated_kernel_headers")
+                                        continue
+                                }
+                                newHeaderLibs = append(newHeaderLibs, entry)
+                        }
+                        return newHeaderLibs
+                }
+                deps.HeaderLibs = rewriteHeaderLibs(deps.HeaderLibs)
+
 	for _, lib := range deps.HeaderLibs {
 		depTag := libraryDependencyTag{Kind: headerLibraryDependency}
 		if inList(lib, deps.ReexportHeaderLibHeaders) {
