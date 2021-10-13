2f491e4e4ae72d99ab6f03fee1f60d1e423b58bf
DennySPb <dennyspb@gmail.com>
2021-10-13 16:00:42 +0300

    soong: whitelist ld.lld and llvm-ar
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If178da808f3c38c4d408c3c53bbacdbdb7c93baa
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -106,6 +106,8 @@ var Configuration = map[string]PathConfig{
 	"jar":     Allowed,
 	"java":    Allowed,
 	"javap":   Allowed,
+	"ld.lld":  Allowed,
+	"llvm-ar": Allowed,
 	"locale":  Allowed,
 	"lsof":    Allowed,
 	"nproc":   Allowed,
