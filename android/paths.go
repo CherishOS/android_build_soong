16bfccc18bd7582d0a54d05cc3d7c2ae6d5381fe
Cosmin Tanislav <demonsingur@gmail.com>
2024-03-20 23:16:26 -0400

    soong: paths: Fix out of tree $OUT
    Change-Id: Ia915a7fcb95c76272fff6d350dc69abacb7b9e00
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- android/paths.go
+++ android/paths.go
@@ -2258,8 +2258,8 @@ func validatePathInternal(allowNinjaVariables bool, pathComponents ...string) (s
 		}
 
 		path := filepath.Clean(path)
-		if path == ".." || strings.HasPrefix(path, "../") || strings.HasPrefix(path, "/") {
-			//return "", fmt.Errorf("Path is outside directory: %s", path)
+		if path == ".." || strings.HasPrefix(path, "../") || i != initialEmpty && strings.HasPrefix(path, "/") {
+			return "", fmt.Errorf("Path is outside directory: %s", path)
 		}
 
 		if i == initialEmpty && pathComponents[i] == "" {
