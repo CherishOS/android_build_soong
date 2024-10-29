781450f520baf6b06ae4f1bcd5cefc6bd821f5ba
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-29 18:05:36 +0800

    build: Add pihook sysprops
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -126,6 +126,9 @@ def parse_args():
   if config["BuildNumber"].startswith("eng."):
     config["BuildNumber"] = config["DateUtc"]
 
+  config["PihooksGmsFp"] = ""
+  config["PihooksGmsModel"] = ""
+
   override_config(config)
 
   append_additional_system_props(args)
@@ -254,6 +257,9 @@ def generate_build_info(args):
   print(f"ro.build.flavor={config['BuildFlavor']}")
 
   print(f"ro.lineage.device={config['LineageDevice']}")
+
+  print(f"persist.sys.pihooks_FINGERPRINT={config['PihooksGmsFp']}")
+  print(f"persist.sys.pihooks_MODEL={config['PihooksGmsModel']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
